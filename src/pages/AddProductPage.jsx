a1c0bd2908e0664ac4dfa141224d86e8a6f6529f
jvillanuevaleal2507-coder <jvillanuevaleal2507@gmail.com>
2026-05-12 09:08:57 -0500

    Update AddProductPage.jsx

--- src/pages/AddProductPage.jsx
+++ src/pages/AddProductPage.jsx
@@ -5,6 +5,9 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 export default function AddProductPage({
+  products,
+  loadProducts,
+}) {
   const [form, setForm] = useState({
     name: "",
     category: "",
